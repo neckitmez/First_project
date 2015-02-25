efb46dfe97791ac954f97d26d3b69be4142fcf98
Nikita Mezenzew <neckitmez@jandex.com>
2015-02-25 23:11:14 +0300

    произошло изменение в H

--- hello_world.java
+++ hello_world.java
@@ -4,8 +4,8 @@ class HelloWorld {
 
 public static void main (String args []) {
 
-System. out. println ("Hello World");
-
+System. out. println ("Hello Nikita\n");
+System. out. println (args[0]);
 }
 
 }
