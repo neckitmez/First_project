f638638dca593258d15eccc1e8802da451338503
Nickita Mesenzew <mezentzew.nickit@yandex.ru>
2015-03-07 23:05:36 +0300

    5 Базавые типы

--- JavaTest.java
+++ JavaTest.java
@@ -1,23 +1,15 @@
-package javatest;
-
-import Test1.Computer;
-
 public class JavaTest {
  
     public static void main(String[] args) {
-                      
-      Computer comp= new Computer();
-      
-      comp.setName("IBM");
-      comp.setRam (-2048);
-      comp.setHdd (-350);
-      
-      
-      
-        comp.on();
-        comp.load();
-        comp.off();
+			short  year;
+			byte day;
+			byte mon;
+			float s; 
+			year = 1998;
+			mon = 11;
+			day = 4;
+			s = (year + mon + day)/3;
+			System.out.print("s="+s);
     }
     
 }
-
