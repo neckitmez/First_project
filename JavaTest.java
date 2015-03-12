3bdef9658b5f550c5c85029f3ba00cf27bbb37ba
Nickita Mesenzew <mezentzew.nickit@yandex.ru>
2015-03-12 23:35:33 +0300

    Вычисление времени

--- JavaTest.java
+++ JavaTest.java
@@ -1,15 +1,21 @@
 public class JavaTest {
- 
-    public static void main(String[] args) {
-			short  year;
-			byte day;
-			byte mon;
-			float s; 
-			year = 1998;
-			mon = 11;
-			day = 4;
-			s = (year + mon + day)/3;
-			System.out.print("s="+s);
-    }
-    
+public static void main(String[] args) {
+short year;
+short day;
+short month;
+short hour;
+year = 16;
+        {
+        if(>0){
+          year = newYear;  
+        }else{
+            System.out.print("The passed value " +newYear+ "can not be negative!");
+        }
+day = (year*365);
+System.out.print("day="+day);
+month = (month*12);
+System.out.print("month="+month);
+hour = (month*8760);
+System.out.print("hour="+hour);
+}
 }
