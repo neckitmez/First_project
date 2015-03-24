8e205d6ccd17be099e5a6360ea07c876a3aff040
Nickita Mesenzew <mezentzew.nickit@yandex.ru>
2015-03-24 17:03:15 +0300

    1 zadanie

--- JavaTest.java
+++ JavaTest.java
@@ -1,32 +1,14 @@
-import java.util.Scanner;
 public class JavaTest {
     public static void main (String [] args){
-        int a;
-        int b;
         int q;
-        int d;
-        int e;
-        int f;
+		int w;
 		int z;
-        Scanner in = new Scanner(System.in);
-        System.out.println("The volume of the first vessel: ");   
-        a=in.nextInt();       
-        System.out.println("The volume of the two vessel: ");            
-        b=in.nextInt();      
-        System.out.println("The desired amount: ");            
-        q=in.nextInt();  
-     	System.out.println("The result: " +cmp (a,b));  
-        d = (a-b);
-		System.out.println("The result: " +cmp (d,q));  
-        e =(b-d);
-		System.out.println("The result: " + cmp (e,q));  
-        f =(a-e);
-		System.out.println("The result: " +cmp (f,q));  
-		z =(f-b);
-		System.out.println("The result: " +cmp (z,q));   
-    }
-	 static boolean cmp (int first, int second){
-		if (first==second) return true;
-		else return false;
+		int c;
+		q = 510;
+		w =100;
+		z = (q/w);
+		c=510%100;
+		System.out.println("The result of division:" +z+"."+c);
+		
 	}
 }
