fd99516f4c80fe5c17ba4672263b25dc500352e1
Nickita Mesenzew <mezentzew.nickit@yandex.ru>
2015-03-28 12:22:30 +0300

    1 zadacha

--- JavaTest.java
+++ JavaTest.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 public class JavaTest {
 	public static void main (String [] args){
-		double a = 12.7;
-		double n = a - (int) a;
-		if (n >= 0.5) {
-		a += 1;
-		} else a = (int) a;
-		System.out.println ("Result rounding = "+( (int)a));
+		int w;
+		int q;
+		Scanner sr = new Scanner(System.in);
+		System.out.print("Enter the dividend = ");
+		w = sr.nextInt();
+		System.out.print("Enter divider = ");
+		q = sr.nextInt();
+		System.out.println("Result of the division = "+((double)w/q));
 	}
 }
